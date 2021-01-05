9c4f56eaec192d03c0742209c526094fee384378
Wolf <weiss.wolfw3@gmail.com>
2021-01-05 12:49:05 -0600

    Added .gitignore, node.js

--- js/search.js
+++ js/search.js
@@ -1,14 +1,17 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI?q=taylor%20swift&pageNumber=1&pageSize=10&autoCorrect=true&safeSearch=true",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "9f3980d96emshfeec0b04d23d3f0p1344dcjsn4727ef038e0f",
-		"x-rapidapi-host": "contextualwebsearch-websearch-v1.p.rapidapi.com"
-	}
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/spelling/AutoComplete',
+  params: {text: 'yeet s'},
+  headers: {
+    'x-rapidapi-key': '9f3980d96emshfeec0b04d23d3f0p1344dcjsn4727ef038e0f',
+    'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+  }
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
 });
