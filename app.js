451eca378ad819617a1cbfc6d11be7e422d86a88
Wolf <weiss.wolfw3@gmail.com>
2021-01-07 10:14:11 -0600

    Added package.json, icon, implemented basic server-client interaction

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ app.get('/main.css', (req, res) => res.sendFile(__dirname + '/css/main.css'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
-  
+
   io.on('connection', (socket) => {
     console.log('a user connected');
+    socket.on('message', (msg) => {
+      console.log(msg);
+      io.emit('message', 'Server Recieved Message');
+    });
   });
-  
+
   http.listen(3000, () => {
     console.log('listening on *:3000');
   });
