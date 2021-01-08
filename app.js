b8dc2c66b6db248f8eb2fad74ff99d8ee1d51fab
Wolf <weiss.wolfw3@gmail.com>
2021-01-07 18:06:23 -0600

    App.js updates

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-app.get('/main.css', (req, res) => res.sendFile(__dirname + '/css/main.css'))
+app.get('/main.css', (req, res) => res.sendFile(__dirname + '/css/main.css'));
+app.get('/favicon.ico', (req, res) => res.sendFile(__dirname + '/favicon.ico'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
