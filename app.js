d3df5fcd0dc95c70d0a91a531ced9f1f8eef4ebd
Wolf Weiss <weiss.wolfw3@gmail.com>
2021-01-11 12:48:52 -0600

    Implemented client search functionality, added axios as npm dependency

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var axios = require("axios").default;
+var options = {
+  method: 'GET',
+  url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/WebSearchAPI', 
+  params: {q: 'taylor swift', pageNumber: '1', pageSize: '10', autoCorrect: 'true'},
+  headers: {
+    'x-rapidapi-key': '9f3980d96emshfeec0b04d23d3f0p1344dcjsn4727ef038e0f',
+    'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+  }
+};
+
 app.get('/main.css', (req, res) => res.sendFile(__dirname + '/css/main.css'));
 app.get('/favicon.ico', (req, res) => res.sendFile(__dirname + '/favicon.ico'));
+app.get('/search.js', (req, res) => res.sendFile(__dirname + '/js/search.js'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
   io.on('connection', (socket) => {
     console.log('a user connected');
-    socket.on('message', (msg) => {
-      console.log(msg);
-      io.emit('message', 'Server Recieved Message');
+    socket.on('query', (qry) => {
+      options.params.q = qry;
+      console.log('Searched for: ' + qry);
+      io.emit('log', qry);
+      axios.request(options).then(function (response) {
+        console.log(response.data);
+        for (let index = 0; index < response.data.value.length; index++) {
+          const element = response.data.value[index];
+          socket.emit('result', element);
+        }
+      }).catch(function (error) {
+        console.error(error);
+      });
+      
     });
+    
   });
 
   http.listen(3000, () => {
--- js/search.js
+++ js/search.js
@@ -1,17 +1,41 @@
-import axios from "axios";
+var socket = io();
+var input = document.getElementById('input');
+var form = document.getElementById('form');
+var results = document.getElementById('results');
 
-const options = {
-  method: 'GET',
-  url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/spelling/AutoComplete',
-  params: {text: 'yeet s'},
-  headers: {
-    'x-rapidapi-key': '9f3980d96emshfeec0b04d23d3f0p1344dcjsn4727ef038e0f',
-    'x-rapidapi-host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('query', input.value);
+    input.value = '';
   }
-};
+});
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+socket.on('result', function(res) {
+  console.log(res);
+  var title = document.createElement('li');
+  var titleLink = document.createElement('a');
+  var url = document.createElement('li');
+  var urlLink = document.createElement('a');
+  var body = document.createElement('li');
+  titleLink.setAttribute('href', res.url);
+  urlLink.setAttribute('href', res.url);
+  title.setAttribute('id', 'title');
+  titleLink.textContent = res.title;
+  title.appendChild(titleLink);
+  results.appendChild(title);
+  urlLink.textContent = res.url;
+  url.appendChild(urlLink);
+  results.appendChild(url);
+  body.textContent = res.body;
+  if (body.textContent.length > 350) {
+    body.textContent = body.textContent.substring(0, 350) + '...';
+  }
+  results.appendChild(body);
+  results.appendChild(document.createElement('br'));
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('log', function(msg) {
+  console.log(msg);
 });
